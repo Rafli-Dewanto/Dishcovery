1aebd2fa1ea30206ff6452737c3c60c989701fcb
Rafli Dewanto <raflidewanto97@gmail.com>
2023-10-09 08:59:29 +0700

    format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dishcovery',
-  description: 'Dishcovery: Where Every Bite Tells a Story!',
-}
+  title: "Dishcovery",
+  description: "Dishcovery: Where Every Bite Tells a Story!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
