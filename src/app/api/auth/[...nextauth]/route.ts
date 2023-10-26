40365034c50d904d61484676fcb2bf817896c6a8
Rafli Dewanto <raflidewanto97@gmail.com>
2023-10-26 19:55:47 +0700

    add edgestore lib

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth";
-import { options } from "./options";
+import { options } from "@/lib/auth-options";
+import NextAuth, { getServerSession } from "next-auth";
 
 export const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
+
+export const getAuthSession = () => getServerSession(options);
