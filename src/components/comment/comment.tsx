2704a11e18a2ca7c5edbcd26bf951d186f788576
HaikalMPPH <haikal.mpph65@gmail.com>
2024-01-12 13:16:25 +0700

    ganti class jadi className, ngilangin avatar user

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,22 +1,20 @@
 import { ArrowDown, ArrowUp } from "lucide-react";
-import UserButton from "../ui/user-button";
 
 export default function Comments() {
     return (
         <div>
-            <div class="flex m-2">
-                <UserButton/>
+            <div className="flex m-2">
                 {/* paragraf comment */}
                 <p></p>
             </div>
-            <div class="grid grid-row-3 m-2">
-                <button class="active:bg-core-accent-50 w-6 justify-center hover:ring hover:ring-core-accent-50">
+            <div className="grid grid-row-3 m-2">
+                <button className="active:bg-core-accent-50 w-6 justify-center hover:ring hover:ring-core-accent-50">
                     <ArrowUp/>
                 </button>
-                <span class="m-2">
+                <span className="m-2">
                     0
                 </span>
-                <button class="active:bg-core-accent-50 w-6 justify-center hover:ring hover:ring-core-accent-50">
+                <button className="active:bg-core-accent-50 w-6 justify-center hover:ring hover:ring-core-accent-50">
                     <ArrowDown/>
                 </button>
             </div>
